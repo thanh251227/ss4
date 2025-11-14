9352745d4f55f125da7c181ebcf88189d4672969
thanh251227 <tranvohuythanh439@gmail.com>
2025-11-14 10:15:34 +0700

    Add files via upload

---
+++ DA201-Bai co ban 3-ss04.sql
@@ -0,0 +1,14 @@
+create database company_db;
+use company_db;
+create table employees(
+employees_id int primary key auto_increment,
+first_name varchar(50) not null unique,
+last_name varchar(50) not null unique
+);
+ALTER TABLE Employees 
+ADD COLUMN HireDate DATE,
+ADD COLUMN Salary INT;
+INSERT INTO Employees (First_Name, Last_Name, HireDate, Salary) VALUES
+('Nguyễn', 'Văn An', '2023-01-15', 15000000),
+('Trần', 'Thị Bình', '2023-03-20', 12000000),
+('Lê', 'Văn Cường', '2023-06-10', 18000000);
